3141dc39e1d36b1dcd191f6b92f70c5127901675
CRB-CyberSec-Dev <163768734+CRB-CyberSec-Dev@users.noreply.github.com>
2024-11-30 17:26:01 +0530

    firebase authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-//Pages
+import { AuthProvider } from "./contexts/authContext";
+
+// Pages
 import Home from "./pages/Home";
+import Header from "./pages/Header";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <AuthProvider>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </AuthProvider>
   );
 };
 

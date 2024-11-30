3ee544270e8b5753a2e66841160ae64e027b2647
Dileepa <113455403+dileepa200108@users.noreply.github.com>
2024-11-30 17:45:42 +0530

    fix app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-
 import { AuthProvider } from "./contexts/authContext";
 
 // Pages
 import Home from "./pages/Home";
-
 import Header from "./pages/Header";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import DeliverySystem from "./pages/delivery_system";
 
 const App = () => {
   return (
@@ -18,18 +17,9 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/delivery_system" element={<DeliverySystem />} />
       </Routes>
     </AuthProvider>
-
-import DeliverySystem from "./pages/delivery_system";
-
-const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/delivery_system" element={<DeliverySystem />} />
-    </Routes>
-
   );
 };
 

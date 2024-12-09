938c176e7fb190ae2eecbc0456b84fa88b1ead3b
CRB-CyberSec-Dev <163768734+CRB-CyberSec-Dev@users.noreply.github.com>
2024-12-09 12:39:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,14 +21,6 @@ app.use(express.json());
 app.use(cors())
 
 
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
 
 
 //Configure app to run in port

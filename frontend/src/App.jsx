b56ae92cee3e0b3eca2e691437ab3814e2d75fec
CRB-CyberSec-Dev <163768734+CRB-CyberSec-Dev@users.noreply.github.com>
2024-12-10 06:40:00 +0530

    Revert "test"
    This reverts commit ec8bb638cd3fcf7317aada571ce2ae2540fe12ff.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,9 @@ import { Route, Routes } from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import SignupWG from "./pages/SignupWG";
+// import Login from "./pages/Login";
+// import Signup from "./pages/Signup";
+// import SignupWG from "./pages/SignupWG";
 
 import CRB from "./pages/crb"
 
@@ -18,10 +18,10 @@ const App = () => {
   return (
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
         <Route path="/deliveryManagement" element={<DeliveryManagement />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signupwg" element={<SignupWG />} />
+        {/* <Route path="/signup" element={<Signup />} />
+        <Route path="/signupwg" element={<SignupWG />} /> */}
         <Route path="/crb" element={<CRB />} />
       </Routes>
   );

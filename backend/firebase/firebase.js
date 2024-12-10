c8be6624398193cdf1f7a91b83f325bae73cfca9
CRB-CyberSec-Dev <163768734+CRB-CyberSec-Dev@users.noreply.github.com>
2024-12-10 06:40:41 +0530

    Revert "fixed firebase"
    This reverts commit 4df3440f9dff7c233d591245bb488b4165f92ae9.

--- backend/firebase/firebase.js
+++ backend/firebase/firebase.js
@@ -1,13 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "@firebase/app";
-import { getAuth } from "@firebase/auth";
-import { getAnalytics } from "@firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-// Load environment variables from .env file
-// require('dotenv').config();
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Access environment variables
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+
+  // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  // authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  // databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  // projectId: "crb2k18",
+  // storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  // measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID, // Optional
+
   apiKey: "AIzaSyDu86sYx4KsyojdCaq7x_CHCMKdrV-vBdQ",
   authDomain: "crb2k18.firebaseapp.com",
   databaseURL: "https://crb2k18.firebaseio.com",
@@ -16,14 +27,13 @@ const firebaseConfig = {
   messagingSenderId: "770326372555",
   appId: "1:770326372555:web:a460ba8ed79450d949bb4e",
   measurementId: "G-V65ZGCNJHP"
+
 };
-
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const analytics = getAnalytics(app); // Optional: Only needed if you're using analytics
+const analytics = getAnalytics(app);
 
-// Export Firebase app and auth
-export { app, auth, analytics };
+const auth = getAuth(app);
+
+export {app,auth};

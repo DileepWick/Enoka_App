870411a184671a9b80b558709714066776e4cd29
Dileepa <113455403+dileepa200108@users.noreply.github.com>
2024-12-11 18:58:07 +0530

    gy

--- frontend/src/contexts/authContext/index.jsx
+++ frontend/src/contexts/authContext/index.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { auth } from "/app/frontend/firebase/auth";
+import { auth } from "../../../firebase/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 
 const AuthContext = React.createContext();

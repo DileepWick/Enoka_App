d67fc946cb05720c5b764e701376dad8f4edb5d4
Dileepa <113455403+dileepa200108@users.noreply.github.com>
2024-12-11 19:29:30 +0530

    das

--- frontend/src/contexts/authContext/index.jsx
+++ frontend/src/contexts/authContext/index.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { auth } from "../../../firebase/firebase";
+import { auth } from "../../../../backend/firebase/auth";
 import { onAuthStateChanged } from "firebase/auth";
 
 const AuthContext = React.createContext();
--- frontend/src/pages/SignupWG.jsx
+++ frontend/src/pages/SignupWG.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { doUpdateUserSignInWithGoogle } from '../../firebase/auth';
+import { doUpdateUserSignInWithGoogle } from '../../../backend/firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios'; // For API requests
 

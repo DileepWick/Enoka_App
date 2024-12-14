b80f41c5c890607df42bfd7f45f7337123c0199b
CRB-CyberSec-Dev <163768734+CRB-CyberSec-Dev@users.noreply.github.com>
2024-12-14 06:59:20 +0530

    Fixed .env for hiding sensitive information and now can compile with firebase.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import EnvironmentPlugin from "vite-plugin-environment";
 
 export default defineConfig({
   plugins: [
     react(),
-    EnvironmentPlugin({
-      VITE_FIREBASE_API_KEY: "",
-      VITE_FIREBASE_AUTH_DOMAIN: "",
-      VITE_FIREBASE_DATABASE_URL: "",
-      VITE_FIREBASE_PROJECT_ID: "",
-      VITE_FIREBASE_STORAGE_BUCKET: "",
-      VITE_FIREBASE_MESSAGING_SENDER_ID: "",
-      VITE_FIREBASE_APP_ID: "",
-      VITE_FIREBASE_MEASUREMENT_ID: "",
-    }),
   ],
+  envPrefix: 'VITE_', // Vite defaults to this, but it's good to be explicit
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    rollupOptions: {
-      external: ['firebase/app', 'firebase/auth', 'firebase/analytics', 'axios']
-    }
   }
 });

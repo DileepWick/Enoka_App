fdca6923c59e5198a452631e18d8e0ccc64f9c1b
Dileepa <113455403+dileepa200108@users.noreply.github.com>
2024-12-20 13:09:44 +0530

    Ui Update add gasket

--- frontend/src/pages/Add_gasket.jsx
+++ frontend/src/pages/Add_gasket.jsx
@@ -209,8 +209,8 @@ export default function AddItemForm() {
               id="part_number"
               name="part_number"
               value={formData.part_number}
+              placeholder="Leave blank if unknown"
               onChange={handleInputChange}
-              required
               size="sm"
               variant="bordered"
               className={`w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black`}
